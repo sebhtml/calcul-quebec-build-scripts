1c64cafa8ef948330db88cabe1e46cc21675b72a
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2013-08-15 11:06:29 -0400

    tigervnc FTW
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- specifications/tigervnc/1.3.0/1.3.0-1.sh
+++ specifications/tigervnc/1.3.0/1.3.0-1.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 source=http://sourceforge.net/projects/tigervnc/files/tigervnc/1.3.0/tigervnc-1.3.0.tar.bz2
+
+archive=$(basename $source)
+
+if test ! -f $archive
+then
+	wget $source
+fi
+
+module load tools/cmake/2.8.10.2
+module load java/jdk1.6.0
+
+
